896935ec339424557733bc8a8e8fb6a4fb542651
Neville <neville.code@gmail.com>
2020-02-25 21:27:19 +1300

    added snake body

---
+++ src/com/neville/game/entities/Body.java
@@ -0,0 +1,31 @@
+package com.neville.game.entities;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+
+public class Body {
+	
+	private int x, y, width, height;
+	
+	public Body(int x, int y, int tileSize) {
+		this.x = x;
+		this.y = y;
+		width = tileSize;
+		height = tileSize;
+		
+	}
+	
+	// updating snake
+	public void tick() {
+		
+	}
+	
+	// drawing on screen
+	public void draw(Graphics g) {
+		g.setColor(Color.BLACK);
+		g.fillRect(x * width, y * height, width, height);
+		g.setColor(Color.GRAY);
+		g.fillRect(x * width + 2, y * width + 2, width - 4, height - 4);
+	}
+}
--- src/com/neville/game/graphics/Screen.java
+++ src/com/neville/game/graphics/Screen.java
@@ -1,28 +1,66 @@
 package com.neville.game.graphics;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.util.ArrayList;
 
 import javax.swing.JPanel;
+
+import com.neville.game.entities.Body;
 
 public class Screen extends JPanel implements Runnable {
 	
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
-	
 	public static final int WIDTH = 800, HEIGHT = 800;
+	private static final long serialVersionUID = 1L;
 	private Thread thread;
 	private boolean running = false;
+	
+	
+	private Body b;
+	private ArrayList<Body> snake;
+	private int x = 10, y = 10;
+	private int size = 5;
+	
+	private boolean right = true, left = false, up = false, down = false;
 	
 	public Screen() {
 		setPreferredSize(new Dimension(WIDTH, HEIGHT));
 		
+		snake = new ArrayList<Body>();
+		
 		start();
 	}
+	
 	public void tick() {
 		System.out.println("Running...");
+		if(snake.size() == 0) {
+			b = new Body(x, y, 10);
+			snake.add(b);
+		}
 	}
+	
+	public void paint(Graphics g) {
+		// drawing grid
+		g.setColor(Color.BLACK);
+		
+		for(int i = 0; i < WIDTH / 10; i++) {
+			g.drawLine( i * 10, 0, i * 10, HEIGHT);
+		}
+		
+		for(int i = 0; i < HEIGHT/ 10; i++) {
+			g.drawLine(0, i * 10, WIDTH, i * 10);
+		}
+		
+		// drawing snake
+		for(int i = 0; i< snake.size(); i++) {
+			snake.get(i).draw(g);
+		}
+	}
+	
 	public void start() {
 		running = true;
 		thread = new Thread(this, "Game Loop");
